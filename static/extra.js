32f57b2f5be97392d3f06fab0c29bb9182849732
sarahhe05 <sarahhe725@gmail.com>
2024-04-06 20:43:39 -0700

    8:43 pm

--- static/extra.js
+++ static/extra.js
@@ -2,31 +2,38 @@
 function toggleDropdown(dropdownId) {
     var dropdownContent = document.getElementById(dropdownId);
     if (dropdownContent.classList.contains("show")) {
-        dropdownContent.classList.remove("show");
+      dropdownContent.classList.remove("show");
     } else {
-        // Close all other dropdowns before opening this one
-        closeAllDropdowns();
-        dropdownContent.classList.add("show");
+      // Close all other dropdowns before opening this one
+      closeAllDropdowns();
+      dropdownContent.classList.add("show");
     }
-}
-
+    }
+  
+/* Set the selected category */
+ function selectCategory(category, dropdownId) {
+   var button = document.getElementById(dropdownId);
+   button.textContent = category; // Update the button text with the selected category
+   toggleDropdown(dropdownId); // Close the dropdown after selection (optional)
+  }
+  
 /* Close all dropdown menus */
-function closeAllDropdowns() {
+  function closeAllDropdowns() {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-        }
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
-}
-
-/* Close the dropdown menu if the user clicks outside of it */
-window.onclick = function(event) {
+  }
+  
+// Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-        closeAllDropdowns();
+      closeAllDropdowns();
     }
-}
+  }
 
 
 var animatedElement = document.createElement('div');
